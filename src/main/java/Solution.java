61a4f2c7c687dd3e86b5de939f75e6b271c597a5
LiquidPlummer <kyle.plummer@revature.com>
2023-03-14 13:03:25 -0400

    notes

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,13 @@
 public class Solution {
     public int maxProfit(int[] prices) {
+        /*
+        This brute force solution is hardly ideal, clocking in with a time complexity of O(n^2). Note the nested
+        for loops. This usually indicates an inefficient solution.
 
+        We pass through the array considering the profit to be made by buying on any given day. We do this by taking
+        that day's price and subtracting it from all future prices, looking for the greatest profit. Once we are
+        finished, we will have considered about n^2 possible outcomes.
+         */
         int potentialProfit = 0;
         for(int i = 0; i < prices.length; i++) {
             int buy = prices[i];
