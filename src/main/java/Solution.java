968918a306ee5dbddc075bf4bd8ee24f53dcd34f
LiquidPlummer <kyle.plummer@revature.com>
2023-03-14 12:59:14 -0400

    notes

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,7 +1,24 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        //write your solution here
+        /*
+        With this solution we are only going to pass through the array once, giving us a time complexity of O(n).
+        We only care about two values as we go: the lowest price, and the highest profit. Our solution will always be
+        the greatest difference between any two elements in the array. As we pass through the array, we track the
+        lowest price, any time we find a lower price we use that price instead. If we didn't find a lower price,
+        we check the profit to be made. Any time we find greater profit, we update the value of maxProfit.
+         */
 
-        return -1;
+        int lowPrice = 10000;
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] < lowPrice) {
+                lowPrice = prices[i];
+            } else if(prices[i] - lowPrice > maxProfit) {
+                maxProfit = prices[i] - lowPrice;
+            }
+        }
+
+        return maxProfit;
     }
 }
