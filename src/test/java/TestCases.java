7f00302fec63f17f746aa99fb450c0718954531e
LiquidPlummer <kyle.plummer@revature.com>
2023-03-14 12:47:38 -0400

    test cases

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,3 +1,24 @@
-public class TestCases {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class TestCases {
+    Solution solution = new Solution();
+
+    @Test
+    public void testCase1() {
+        int[] prices = new int[]{7,1,5,3,6,4};
+        int expected = 5;
+
+        Assertions.assertEquals(expected, solution.maxProfit(prices));
+
+    }
+
+    @Test
+    public void testCase2() {
+        int[] prices = new int[]{7,6,4,3,1};
+        int expected = 0;
+
+        Assertions.assertEquals(expected, solution.maxProfit(prices));
+
+    }
 }
